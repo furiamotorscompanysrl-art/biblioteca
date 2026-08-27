c6da7401d392a053d046b3188a3b8de38faa1a2c
VC76884070 <vaniacarrasco76884070@gmail.com>
2026-08-27 09:26:34 -0400

    deploy

--- arteydis/asgi.py
+++ arteydis/asgi.py
@@ -2,6 +2,6 @@
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arteydis.settings')  # Cambiar a arteydis
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arteydis.settings')  # Cambiar a arteydisddddd
 
 application = get_asgi_application()
